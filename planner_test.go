0df6838975e04e2f27b1ab0e407262cd579d53e8
rosswf <ross@rossw.co.uk>
2022-06-26 20:47:50 +0100

    Add test for marking a task complete

--- planner_test.go
+++ planner_test.go
@@ -8,12 +8,27 @@ import (
 )
 
 func TestTasks(t *testing.T) {
+	taskList := planner.Tasks{}
+
 	t.Run("A task is added to the task list", func(t *testing.T) {
-		taskList := planner.Tasks{}
 		taskList.Add("Task 1")
 
 		got := taskList.GetAll()
 		want := []planner.Task{{Name: "Task 1", Complete: false}}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %+v, want %+v", got, want)
+		}
+	})
+
+	t.Run("A task is marked as completed", func(t *testing.T) {
+		err := taskList.Complete(1)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		got := taskList.GetAll()
+		want := []planner.Task{{Name: "Task 1", Complete: true}}
 
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %+v, want %+v", got, want)
