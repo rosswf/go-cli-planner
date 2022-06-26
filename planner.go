f7718acb5634528fc438c3e001ff8af76c1fafde
rosswf <ross@rossw.co.uk>
2022-06-26 20:52:38 +0100

    Add complete method for tasks

--- planner.go
+++ planner.go
@@ -1,5 +1,7 @@
 package planner
 
+import "errors"
+
 type Task struct {
 	Name     string
 	Complete bool
@@ -9,6 +11,17 @@ type Tasks []Task
 
 func (t *Tasks) Add(name string) {
 	*t = append(*t, Task{Name: name, Complete: false})
+}
+
+func (t Tasks) Complete(id int) error {
+	id-- // slice is zero indexed
+
+	if id > len(t) {
+		return errors.New("No task with that id")
+	}
+
+	t[id].Complete = true
+	return nil
 }
 
 func (t Tasks) GetAll() []Task {
