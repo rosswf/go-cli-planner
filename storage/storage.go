7671345510aaa2c7ae390b22e992707be004d1ac
rosswf <ross@rossw.co.uk>
2022-07-03 17:01:16 +0100

    Tidy up SQL storage

--- storage/storage.go
+++ storage/storage.go
@@ -12,9 +12,14 @@ type Sqlite3TaskStorage struct {
 }
 
 func CreateSqlite3TaskStorage(location string) (*Sqlite3TaskStorage, error) {
-	db, _ := sql.Open("sqlite3", location) // Come back to this future me
-	sqlStmt := `CREATE TABLE IF NOT EXISTS tasks (id INTEGER not null primary key, name TEXT, complete BOOL);`
-	_, err := db.Exec(sqlStmt)
+	db, err := sql.Open("sqlite3", location)
+	if err != nil {
+		return &Sqlite3TaskStorage{}, err
+	}
+	sqlStmt := `CREATE TABLE IF NOT EXISTS tasks
+(id INTEGER not null primary key, name TEXT, complete BOOL);`
+
+	_, err = db.Exec(sqlStmt)
 	if err != nil {
 		return nil, err
 	}
@@ -26,16 +31,11 @@ func (s *Sqlite3TaskStorage) Close() {
 }
 
 func (s *Sqlite3TaskStorage) Add(task *planner.Task) error {
-	tx, _ := s.conn.Begin()
-	stmt, _ := tx.Prepare("INSERT INTO tasks(name, complete) values(?, ?)")
-	defer stmt.Close()
-	result, _ := stmt.Exec(task.Name, task.Complete)
-	err := tx.Commit()
+	sqlStmt := "INSERT INTO tasks(name, complete) values(?, ?)"
+	_, err := s.conn.Exec(sqlStmt, task.Name, task.Complete)
 	if err != nil {
 		return err
 	}
-	id, err := result.LastInsertId()
-	task.Id = planner.TaskId(id)
 	return err
 }
 
@@ -69,25 +69,13 @@ func (s *Sqlite3TaskStorage) GetTask(id planner.TaskId) (*planner.Task, error) {
 }
 
 func (s *Sqlite3TaskStorage) ToggleStatus(id planner.TaskId) error {
-	tx, err := s.conn.Begin()
-	if err != nil {
-		return err
-	}
-	stmt, err := tx.Prepare("UPDATE tasks SET complete = CASE WHEN complete = true THEN false ELSE true END WHERE id=?")
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
+	sqlStmt := `UPDATE tasks SET complete = CASE WHEN complete = true 
+THEN false ELSE true END WHERE id=?`
 
-	_, err = stmt.Exec(id)
+	_, err := s.conn.Exec(sqlStmt, id)
 	if err != nil {
 		return err
 	}
-	err = tx.Commit()
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
 
@@ -108,21 +96,8 @@ func (s *Sqlite3TaskStorage) GetOutstanding() ([]planner.Task, error) {
 }
 
 func (s *Sqlite3TaskStorage) Delete(id planner.TaskId) error {
-	tx, err := s.conn.Begin()
-	if err != nil {
-		return err
-	}
-	stmt, err := tx.Prepare("DELETE FROM tasks WHERE id=?")
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-
-	_, err = stmt.Exec(id)
-	if err != nil {
-		return err
-	}
-	err = tx.Commit()
+	sqlStmt := "DELETE FROM tasks WHERE id=?"
+	_, err := s.conn.Exec(sqlStmt, id)
 	if err != nil {
 		return err
 	}
